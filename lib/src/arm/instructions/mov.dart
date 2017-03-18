61479dcb392e661346b82ad99ff307669c04240c
Matan Lurey <matan@lurey.org>
2017-03-17 20:02:17 -0700

    Implement a bunch of instructions.

--- lib/src/arm/instructions/mov.dart
+++ lib/src/arm/instructions/mov.dart
@@ -1,11 +1,34 @@
 part of arm7_tdmi.src.arm.compiler;
 
+/// Implements the 'Move' Instruction.
 class _ArmInstruction$MOV extends Instruction {
+  /// Second operand of the instruction.
+  final int op2;
+
+  /// Destination register.
+  final int rd;
+
+  /// Determines whether the instruction updates the CPSR.
+  final bool s;
+
   const _ArmInstruction$MOV({
     @required ArmCondition condition,
+    @required this.op2,
+    @required this.rd,
+    @required this.s,
   })
       : super._(condition: condition, name: 'MOV');
 
   @override
-  noSuchMethod(_) => super.noSuchMethod(_);
+  int execute(Cpu cpu) {
+    if (condition.pass(cpu.cpsr)) {
+      final r = cpu.gprs[rd] = op2.toUnsigned(32);
+      if (s) {
+        cpu.cpsr
+          ..n = r > 0x7FFFFFFF
+          ..z = r == 0;
+      }
+    }
+    return 1;
+  }
 }
--- lib/src/arm/instructions/mrs.dart
+++ lib/src/arm/instructions/mrs.dart
@@ -1,11 +1,25 @@
 part of arm7_tdmi.src.arm.compiler;
 
+/// Implements the 'Move PSR to General-Purpose Register' Instruction.
 class _ArmInstruction$MRS extends Instruction {
+  /// Destination register.
+  final int rd;
+
+  /// Whether to move the SPSR, otherwise the CPSR is used.
+  final bool spsr;
+
   const _ArmInstruction$MRS({
     @required ArmCondition condition,
+    @required this.rd,
+    @required this.spsr,
   })
       : super._(condition: condition, name: 'MRS');
 
   @override
-  noSuchMethod(_) => super.noSuchMethod(_);
+  int execute(Cpu cpu) {
+    if (condition.pass(cpu.cpsr)) {
+      cpu.gprs[rd] = spsr ? cpu.spsr.value : cpu.cpsr.value;
+    }
+    return 1;
+  }
 }
