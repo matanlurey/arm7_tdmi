d83ad58a8eaafa0264bbd07ea4fef0dc2f22d8a8
kharland <kendaljharland@gmail.com>
2017-05-29 19:45:00 -0700

    Decode remaining instruction formats.

--- test/arm/decoder_test.dart
+++ test/arm/decoder_test.dart
@@ -9,7 +9,6 @@ void main() {
   // ARM instructions generated with arm-none-eabi-as.
   // https://github.com/smiley22/ARM.JS/blob/gh-pages/Tests/Test.Cpu.ts.
   const [
-    /** Data Processing instructions **/
     // adds r4, r0, r2
     const Tuple2<int, String>(
       0xe0904002,
@@ -30,35 +29,121 @@ void main() {
       0xe1510000,
       'CMP',
     ),
+    // bl   14
+    const Tuple2<int, String>(
+      0xeb000001,
+      'BL',
+    ),
+    // bx   lr
+    const Tuple2<int, String>(
+      0xe12fff1e,
+      'BX',
+    ),
     // bic  r9, r9, #3
     const Tuple2<int, String>(
       0xe3c99003,
       'BIC',
+    ),
+    // mcr  15, 0, r0, cr7, cr10, {4}
+    const Tuple2<int, String>(
+      0xee070f9a,
+      'MCR',
+    ),
+    // ldr  r2, [pc, #56]
+    const Tuple2<int, String>(
+      0xe59f2038,
+      'LDR',
+    ),
+    // msr  CPSR_fc, r0
+    const Tuple2<int, String>(
+      0xe129f000,
+      'MSR',
+    ),
+    // mcr  15, 0, r0, cr8, cr7, {0}
+    const Tuple2<int, String>(
+      0xee080f17,
+      'MCR',
+    ),
+    // ldr  r2, [pc, #56]
+    const Tuple2<int, String>(
+      0xe59f2038,
+      'LDR',
     ),
     // bic  r3, r3, #1
     const Tuple2<int, String>(
       0xe3c33001,
       'BIC',
     ),
-
+    // mcr  15, 0, r3, cr1, cr0, {0}
+    const Tuple2<int, String>(
+      0xee013f10,
+      'MCR',
+    ),
     // mov  pc, r2
     const Tuple2<int, String>(
       0xe1a0f002,
       'MOV',
     ),
-
+    // ldr  ip, [pc, #44]
+    const Tuple2<int, String>(
+      0xe59fc02c,
+      'LDR',
+    ),
     // mov  r0, #243
     const Tuple2<int, String>(
       0xe3a000f3,
       'MOV',
     ),
-
+    // str  r0, [ip, #31]
+    const Tuple2<int, String>(
+      0xe58c001f,
+      'STR',
+    ),
+    // bl   0 <main>
+    const Tuple2<int, String>(
+      0xebfffffe,
+      'BL',
+    ),
+    // b    58 <.text + 0x58>
+    const Tuple2<int, String>(
+      0xeafffffe,
+      'B',
+    ),
+    // ldr  r1, [r0]
+    const Tuple2<int, String>(
+      0xe5901000,
+      'LDR',
+    ),
     // cmp  r1, #0
     const Tuple2<int, String>(
       0xe3510000,
       'CMP',
     ),
-
+    // bne  6c <.text + 0x6c>
+    const Tuple2<int, String>(
+      0x1a000000,
+      'B',
+    ),
+    // str  r1, [r0]
+    const Tuple2<int, String>(
+      0xe5801000,
+      'STR',
+    ),
+    // ldr  r1, [r0, #8]
+    const Tuple2<int, String>(
+      0xe5901008,
+      'LDR',
+    ),
+    // ldr  r2, [r0, #12]
+    const Tuple2<int, String>(
+      0xe590200c,
+      'LDR',
+    ),
+    // ldrb r3, [r1], #1
+    const Tuple2<int, String>(
+      0xe4d13001,
+      'LDR',
+    ),
     // andeq r0, r0, r8, asr r0
     const Tuple2<int, String>(
       0x00000058,
@@ -88,77 +173,6 @@ void main() {
     const Tuple2<int, String>(
       0x01180306,
       'TST',
-    ),
-
-    /** Branch and Branch with Link instructions **/
-    // bl   14
-    const Tuple2<int, String>(
-      0xeb000001,
-      'BL',
-    ),
-    // bl   0 <main>
-    const Tuple2<int, String>(
-      0xebfffffe,
-      'BL',
-    ),
-    // bx   lr
-    const Tuple2<int, String>(
-      0xe12fff1e,
-      'BX',
-    ),
-    // bne  6c <.text + 0x6c>
-    const Tuple2<int, String>(
-      0x1a000000,
-      'B',
-    ),
-    // b    58 <.text + 0x58>
-    const Tuple2<int, String>(
-      0xeafffffe,
-      'B',
-    ),
-
-    /** Coprocessor register transfers **/
-    // mcr  15, 0, r0, cr7, cr10, {4}
-    const Tuple2<int, String>(
-      0xee070f9a,
-      'MCR',
-    ),
-    // mcr  15, 0, r0, cr8, cr7, {0}
-    const Tuple2<int, String>(
-      0xee080f17,
-      'MCR',
-    ),
-    // mcr  15, 0, r3, cr1, cr0, {0}
-    const Tuple2<int, String>(
-      0xee013f10,
-      'MCR',
-    ),
-
-    /** Loads and stores **/
-    // ldr  r1, [r0]
-    const Tuple2<int, String>(
-      0xe5901000,
-      'LDR',
-    ),
-    // ldr  r2, [pc, #56]
-    const Tuple2<int, String>(
-      0xe59f2038,
-      'LDR',
-    ),
-    // ldr  ip, [pc, #44]
-    const Tuple2<int, String>(
-      0xe59fc02c,
-      'LDR',
-    ),
-    // ldr  r2, [r0, #12]
-    const Tuple2<int, String>(
-      0xe590200c,
-      'LDR',
-    ),
-    // ldrb r3, [r1], #1
-    const Tuple2<int, String>(
-      0xe4d13001,
-      'LDR',
     ),
   ].forEach((tuple) {
     final bits = tuple.item1;
